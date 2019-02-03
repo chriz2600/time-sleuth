5ea442d138036b8251db02fca9798076549c0ff5
Christof Harnischmacher <ch@cm4all.com>
2019-02-03 23:12:56 +0100

    averaging working

--- source/video.v
+++ source/video.v
@@ -124,79 +124,205 @@ module video(
                 lagdisplay_end_pos <= (videoMode.v_lag_start + (12'd16 << videoMode.v_lag_divider));
                 char_addr_base <= lagdisplay_addr + (8'h_30 << 4);
             end
+            2: begin
+                trigger_write_lag <= 1;
+            end
+            3: begin
+                trigger_write_lag <= 0;
+            end
         endcase
     end
 
+    reg trigger_write_lag;
+    reg [5:0] write_lag_counter;
+
+    task idwc;
+        begin
+            write_lag_counter <= write_lag_counter + 1'b1;
+        end
+    endtask
+
     always @(posedge clock) begin
-        case (counterX)
-            3: lagdisplay_line_out <= lagdisplay_line;
-            // bcdcount
-            4: char_addr <= char_addr_base + (bcdcount[3:0] << 4);
-            5: char_addr <= char_addr_base + (bcdcount[7:4] << 4);
-            6: char_addr <= char_addr_base + (bcdcount[11:8] << 4);
-            7: begin 
-                lagdisplay_line_out[199:192] <= char_data;
-                char_addr <= char_addr_base + (bcdcount[15:12] << 4);
-            end
-            8: begin 
-                lagdisplay_line_out[207:200] <= char_data;
-                char_addr <= char_addr_base + (bcdcount[19:16] << 4);
-            end
-            9: lagdisplay_line_out[223:216] <= char_data;
-            10: lagdisplay_line_out[231:224] <= char_data;
-            11: lagdisplay_line_out[239:231] <= char_data;
+        if (trigger_write_lag) begin
+            write_lag_counter <= 0;
+        end else begin
+            case (write_lag_counter)
+                0: begin
+                    lagdisplay_line_out <= lagdisplay_line;
+                    idwc();
+                end
+                // bcdcount
+                1: begin
+                    if (bcdcount[19:0] == MAX_BCDCOUNT) begin
+                        write_lag_counter <= write_lag_counter + 6'd9;
+                    end else begin
+                        idwc();
+                    end
+                end
+                2: begin
+                    char_addr <= char_addr_base + (bcdcount[3:0] << 4);
+                    idwc();
+                end
+                3: begin
+                    char_addr <= char_addr_base + (bcdcount[7:4] << 4);
+                    idwc();
+                end
+                4: begin
+                    char_addr <= char_addr_base + (bcdcount[11:8] << 4);
+                    idwc();
+                end
+                5: begin
+                    lagdisplay_line_out[199:192] <= char_data;
+                    char_addr <= char_addr_base + (bcdcount[15:12] << 4);
+                    idwc();
+                end
+                6: begin
+                    lagdisplay_line_out[207:200] <= char_data;
+                    char_addr <= char_addr_base + (bcdcount[19:16] << 4);
+                    idwc();
+                end
+                7: begin
+                    lagdisplay_line_out[223:216] <= char_data;
+                    idwc();
+                end
+                8: begin
+                    lagdisplay_line_out[231:224] <= char_data;
+                    idwc();
+                end
+                9: begin
+                    lagdisplay_line_out[239:231] <= char_data;
+                    idwc();
+                end
+                // bcdcount_min
+                10: begin
+                    if (bcdcount[39:20] == MAX_BCDCOUNT) begin
+                        write_lag_counter <= write_lag_counter + 6'd9;
+                    end else begin
+                        idwc();
+                    end
+                end
+                11: begin
+                    char_addr <= char_addr_base + (bcdcount[23:20] << 4);
+                    idwc();
+                end
+                12: begin
+                    char_addr <= char_addr_base + (bcdcount[27:24] << 4);
+                    idwc();
+                end
+                13: begin
+                    char_addr <= char_addr_base + (bcdcount[31:28] << 4);
+                    idwc();
+                end
+                14: begin
+                    lagdisplay_line_out[143:136] <= char_data;
+                    char_addr <= char_addr_base + (bcdcount[35:32] << 4);
+                    idwc();
+                end
+                15: begin
+                    lagdisplay_line_out[152:144] <= char_data;
+                    char_addr <= char_addr_base + (bcdcount[39:36] << 4);
+                    idwc();
+                end
+                16: begin
+                    lagdisplay_line_out[167:160] <= char_data;
+                    idwc();
+                end
+                17: begin
+                    lagdisplay_line_out[175:168] <= char_data;
+                    idwc();
+                end
+                18: begin
+                    lagdisplay_line_out[183:176] <= char_data;
+                    idwc();
+                end
 
-            // bcdcount_min
-            13: char_addr <= char_addr_base + (bcdcount[23:20] << 4);
-            14: char_addr <= char_addr_base + (bcdcount[27:24] << 4);
-            15: char_addr <= char_addr_base + (bcdcount[31:28] << 4);
-            16: begin 
-                lagdisplay_line_out[143:136] <= char_data;
-                char_addr <= char_addr_base + (bcdcount[35:32] << 4);
-            end
-            17: begin 
-                lagdisplay_line_out[152:144] <= char_data;
-                char_addr <= char_addr_base + (bcdcount[39:36] << 4);
-            end
-            18: lagdisplay_line_out[167:160] <= char_data;
-            19: lagdisplay_line_out[175:168] <= char_data;
-            20: lagdisplay_line_out[183:176] <= char_data;
+                // bcdcount_max
+                19: begin
+                    if (bcdcount[59:40] == 0) begin
+                        write_lag_counter <= write_lag_counter + 6'd9;
+                    end else begin
+                        idwc();
+                    end
+                end
+                20: begin
+                    char_addr <= char_addr_base + (bcdcount[43:40] << 4);
+                    idwc();
+                end
+                21: begin
+                    char_addr <= char_addr_base + (bcdcount[47:44] << 4);
+                    idwc();
+                end
+                22: begin
+                    char_addr <= char_addr_base + (bcdcount[51:48] << 4);
+                    idwc();
+                end
+                23: begin
+                    lagdisplay_line_out[87:80] <= char_data;
+                    char_addr <= char_addr_base + (bcdcount[55:52] << 4);
+                    idwc();
+                end
+                24: begin
+                    lagdisplay_line_out[95:88] <= char_data;
+                    char_addr <= char_addr_base + (bcdcount[59:56] << 4);
+                    idwc();
+                end
+                25: begin
+                    lagdisplay_line_out[111:104] <= char_data;
+                    idwc();
+                end
+                26: begin
+                    lagdisplay_line_out[119:112] <= char_data;
+                    idwc();
+                end
+                27: begin
+                    lagdisplay_line_out[127:120] <= char_data;
+                    idwc();
+                end
 
-            // bcdcount_max
-            22: char_addr <= char_addr_base + (bcdcount[43:40] << 4);
-            23: char_addr <= char_addr_base + (bcdcount[47:44] << 4);
-            24: char_addr <= char_addr_base + (bcdcount[51:48] << 4);
-            25: begin 
-                lagdisplay_line_out[87:80] <= char_data;
-                char_addr <= char_addr_base + (bcdcount[55:52] << 4);
-            end
-            26: begin 
-                lagdisplay_line_out[95:88] <= char_data;
-                char_addr <= char_addr_base + (bcdcount[59:56] << 4);
-            end
-            27: lagdisplay_line_out[111:104] <= char_data;
-            28: lagdisplay_line_out[119:112] <= char_data;
-            29: lagdisplay_line_out[127:120] <= char_data;
-
-            // avg_counter
-            31: char_addr <= char_addr_base + (bcdcount[63:60] << 4);
-            32: char_addr <= char_addr_base + (bcdcount[67:64] << 4);
-            33: char_addr <= char_addr_base + (bcdcount[71:68] << 4);
-            34: begin 
-                lagdisplay_line_out[31:24] <= char_data;
-                char_addr <= char_addr_base + (bcdcount[75:72] << 4);
-            end
-            35: begin 
-                lagdisplay_line_out[39:32] <= char_data;
-                char_addr <= char_addr_base + (bcdcount[79:76] << 4);
-            end
-            36: lagdisplay_line_out[55:48] <= char_data;
-            37: lagdisplay_line_out[63:56] <= char_data;
-            38: lagdisplay_line_out[71:64] <= char_data;
-
-            // output pipeline
-            // 40: lagdisplay_line_out_2 <= lagdisplay_line_out;
-        endcase
+                // avg_counter
+                28: begin
+                    if (bcdcount[79:60] == MAX_BCDCOUNT) begin
+                        write_lag_counter <= write_lag_counter + 6'd9;
+                    end else begin
+                        idwc();
+                    end
+                end
+                29: begin
+                    char_addr <= char_addr_base + (bcdcount[63:60] << 4);
+                    idwc();
+                end
+                30: begin
+                    char_addr <= char_addr_base + (bcdcount[67:64] << 4);
+                    idwc();
+                end
+                31: begin
+                    char_addr <= char_addr_base + (bcdcount[71:68] << 4);
+                    idwc();
+                end
+                32: begin
+                    lagdisplay_line_out[31:24] <= char_data;
+                    char_addr <= char_addr_base + (bcdcount[75:72] << 4);
+                    idwc();
+                end
+                33: begin
+                    lagdisplay_line_out[39:32] <= char_data;
+                    char_addr <= char_addr_base + (bcdcount[79:76] << 4);
+                    idwc();
+                end
+                34: begin
+                    lagdisplay_line_out[55:48] <= char_data;
+                    idwc();
+                end
+                35: begin
+                    lagdisplay_line_out[63:56] <= char_data;
+                    idwc();
+                end
+                36: begin
+                    lagdisplay_line_out[71:64] <= char_data;
+                    idwc();
+                end
+            endcase
+        end
     end 
 
     /* visible area counter */
