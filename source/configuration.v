c761034f6d8cbceaa21c0c34dd17fd7810e5058e
Christof Harnischmacher <ch@cm4all.com>
2019-02-08 23:34:58 +0100

    using internal pull ups

--- source/configuration.v
+++ source/configuration.v
@@ -11,13 +11,11 @@ module configuration(
     always @(posedge clock) begin
         prev_config_data <= config_data;
         case (config_in)
-            5'b_00001: config_data <= `MODE_1080p; // switch 1
-            5'b_00011: config_data <= `MODE_1080p; // switch 1 legacy
-            5'b_00010: config_data <= `MODE_1080i; // switch 2
-            5'b_00100: config_data <= `MODE_720p;  // switch 3
-            5'b_00110: config_data <= `MODE_720p;  // switch 3 legacy
-            5'b_01000: config_data <= `MODE_480p;  // switch 4
-            5'b_10000: config_data <= `MODE_480i;  // switch 5
+            5'b_11110: config_data <= `MODE_1080p; // switch 1
+            5'b_11101: config_data <= `MODE_1080i; // switch 2
+            5'b_11011: config_data <= `MODE_720p;  // switch 3
+            5'b_10111: config_data <= `MODE_480p;  // switch 4
+            5'b_01111: config_data <= `MODE_480i;  // switch 5
             default: config_data <= `DEFAULT_FB_MODE;
         endcase
     end
